ef45d367a5e9acf46da666c178c4d032ab9251e7
Trenton Choate <38508613+Trenton44@users.noreply.github.com>
2022-09-15 12:40:51 -0500

    added login link to react file

--- react_frontend/src/index.js
+++ react_frontend/src/index.js
@@ -4,6 +4,6 @@ import ReactDOM from 'react-dom/client';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <h1>Hello World!</h1>
+    <a href="https://www.bungie.net/en/OAuth/Authorize">Login</a>
   </React.StrictMode>
 );
