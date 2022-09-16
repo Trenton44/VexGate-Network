4e781632be13d7ece9a50060c84b2f4859bd4889
Trenton44 <38508613+Trenton44@users.noreply.github.com>
2022-09-16 08:08:09 +0000

    Updated server.js -Added prettify function to make await error handling simpler and prettier -functions interfacing server and api are now under api_interface.js -wrapper functions for API calls are now under d2_api_wrapper.js Began adding wrapper functions for d2 api

--- fastify_backend/bungie_api/d2_api_wrapper.js
+++ fastify_backend/bungie_api/d2_api_wrapper.js
@@ -1,1 +1,14 @@
-const bungieAuthO = require('./authO.js');
+const D2APIInterface = require('./api_interface.js');
+const bungie_root = "https://www.bungie.net";
+const api_root = bungie_root+"/Platform";
+
+function GetCredentialTypesForTargetAccount(membership_id, token){
+    let path = api_root+"/User/GetCredentialTypesForTargetAccount/"+membership_id+"/";
+    return D2APIInterface.APIGet(path, token);
+}
+
+function GetMembershipDataById(membership_id, token){
+    let path = api_root+"/User/GetMembershipsById/"+membership_id+"/-1/";
+    return D2APIInterface.APIGet(path, token);
+}
+module.exports = {GetCredentialTypesForTargetAccount, GetMembershipDataById};
--- fastify_backend/server.js
+++ fastify_backend/server.js
@@ -1,14 +1,19 @@
 //Source code for the backend itself.
 
+
+const prettify = (promise) => {
+    return promise
+    .then(result => ([result, undefined]))
+    .catch(error => ([undefined, error]));
+};
+
 require('dotenv').config({path: '.env'});
 const fs = require('fs')
 const path = require ('path');
-const D2API = require('./bungie_api/d2_api_wrapper.js');
-const bungieAuthO = require('./bungie_api/authO.js');
-
+const D2APIInterface = require('./bungie_api/api_interface.js');
+const D2APIWrapper = require('./bungie_api/d2_api_wrapper.js');
 let compiled_front_end = path.join(__dirname, '..', '/react_frontend/build');
-const bungie_root = "https://www.bungie.net";
-const api_root = bungie_root+"/Platform";
+
 
 const server_app = require('fastify')({
     logger: true,
@@ -19,9 +24,7 @@ const server_app = require('fastify')({
         cert: fs.readFileSync("/etc/pki/tls/certs/fastify_backend_selfsigned.crt")
     }
 });
-server_app.register(require('@fastify/static'), {
-    root: compiled_front_end,
-});
+server_app.register(require('@fastify/static'), { root: compiled_front_end });
 server_app.register(require('@fastify/cookie'));
 server_app.register(require('@fastify/session'), { secret: process.env.SESSION_SECRET });
 
@@ -36,29 +39,28 @@ server_app.addHook('preHandler', (request, reply, next) => {
 
 server_app.get('/', function(request, reply){ reply.sendFile("index.html"); });
 
-server_app.get('/bnet_auth_request', function(request, reply) {
-    let redirect = bungieAuthO.generateAuthORedirect(request);
+server_app.get('/bnet_auth_request', async (request, reply) => {
+    let redirect = D2APIInterface.generateAuthORedirect(request);
     reply.code(303).redirect(encodeURI(redirect));
-
 });
 server_app.get('/bnet_auth_response', async (request, reply) => {
-    if( request.session.state != decodeURIComponent(request.query.state)){
+    if(request.session.state != decodeURIComponent(request.query.state)){
         request.session.destroy();
-        //This is where we would return an error object
-        return false; 
+        //needs to return an error based on a schema. schemas are up next for implementation
+        return false;
     }
-    await bungieAuthO.APITokenRequest(decodeURIComponent(request.query.code))
-    .then(function(result){
-        bungieAuthO.processAPITokenResponse(request, result.data);
-        return true;
-    }).catch(function(error){
-        return error;
-    });
+    let [result, error] = await prettify(D2APIInterface.APITokenRequest(decodeURIComponent(request.query.code)));
+    if(error != undefined){ return "error"; }
+    D2APIInterface.processAPITokenResponse(request, result.data);
     reply.code(303).redirect("/");
 });
 
 server_app.get('/test', async (request, reply) => {
-    return {token: request.session.authData.access_token}; 
+    let id = request.session.user.membership_id;
+    let token = request.session.authData.access_token;
+    let [result, error] = await prettify(D2APIWrapper.GetMembershipDataById(id, token));
+    if(error != undefined){ return error; }
+    return result.data;
 });
 
 server_app.listen({ port: process.env.PORT_NUMBER, host: '0.0.0.0' }, function(err, address){ if(err){ process.exit(1); } });
