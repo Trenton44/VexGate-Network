176d8fc9fe8c363c7dc76aa0e88ff08e6ef72e0a
Trenton44 <38508613+Trenton44@users.noreply.github.com>
2022-09-16 17:06:59 +0000

    Added some api wrapper funcstions, and some extra checking logic

--- fastify_backend/bungie_api/d2_api_wrapper.js
+++ fastify_backend/bungie_api/d2_api_wrapper.js
@@ -2,13 +2,27 @@ const D2APIInterface = require('./api_interface.js');
 const bungie_root = "https://www.bungie.net";
 const api_root = bungie_root+"/Platform";
 
-function GetCredentialTypesForTargetAccount(membership_id, token){
+
+function GetDestinyManifest(){
+    let path = api_root+"/Destiny2/Manifest/";
+    return D2APIInterface.APIGet(path);
+};
+
+function GetCredentialTypesForTargetAccount(token, membership_id){
     let path = api_root+"/User/GetCredentialTypesForTargetAccount/"+membership_id+"/";
     return D2APIInterface.APIGet(path, token);
 }
 
-function GetMembershipDataById(membership_id, token){
-    let path = api_root+"/User/GetMembershipsById/"+membership_id+"/-1/";
+function GetMembershipDataById(token, membership_id, membership_type){
+    if(!membership_type) membership_type = "-1"; // -1 is the enum value for all membership types. hopefully remove this hardcoding later.
+    let path = api_root+"/User/GetMembershipsById/"+membership_id+"/"+membership_type+"/";
     return D2APIInterface.APIGet(path, token);
-}
-module.exports = {GetCredentialTypesForTargetAccount, GetMembershipDataById};
+};
+
+function GetLinkedProfiles(token, membership_id, membership_type){
+    if(!membership_type) membership_type = "-1"; // -1 is the enum value for all membership types. hopefully remove this hardcoding later.
+    let path = api_root+"/Destiny2/"+membership_type+"/Profile/"+membership_id+"/LinkedProfiles/";
+    return D2APIInterface.APIGet(path, token);
+};
+
+module.exports = {GetDestinyManifest, GetCredentialTypesForTargetAccount, GetMembershipDataById, GetLinkedProfiles};
