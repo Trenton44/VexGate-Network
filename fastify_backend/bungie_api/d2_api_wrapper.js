0078ad8f44747c59a9eb6c716b977b8a5dbcabaa
Trenton44 <38508613+Trenton44@users.noreply.github.com>
2022-09-19 00:51:46 +0000

    Added more API Functions to wrapper file

--- fastify_backend/bungie_api/d2_api_wrapper.js
+++ fastify_backend/bungie_api/d2_api_wrapper.js
@@ -3,20 +3,27 @@ const bungie_root = "https://www.bungie.net";
 const api_root = bungie_root+"/Platform";
 
 
+// Functions categorized as "User" on the bnet API docs
+function GetCredentialTypesForTargetAccount(token, membership_id){
+    let path = api_root+"/User/GetCredentialTypesForTargetAccount/"+membership_id+"/";
+    return D2APIInterface.APIGet(path, token);
+};
+
+// Functions categorized as "Destiny2" on the bnet API docs
 function GetDestinyManifest(){
     let path = api_root+"/Destiny2/Manifest/";
     return D2APIInterface.APIGet(path);
 };
 
-function GetCredentialTypesForTargetAccount(token, membership_id){
-    let path = api_root+"/User/GetCredentialTypesForTargetAccount/"+membership_id+"/";
-    return D2APIInterface.APIGet(path, token);
-}
-
-function GetMembershipDataById(token, membership_id, membership_type){
+function SearchDestinyPlayerByBungieName(token, display_name, display_code, membership_type){
     if(!membership_type) membership_type = "-1"; // -1 is the enum value for all membership types. hopefully remove this hardcoding later.
-    let path = api_root+"/User/GetMembershipsById/"+membership_id+"/"+membership_type+"/";
-    return D2APIInterface.APIGet(path, token);
+    let body = {
+        displayName: display_name,
+        displayNameCode: display_code
+    };
+    let path = api_root+"/Destiny2/SearchDestinyPlayerByBungieName/"+membership_type+"/";
+    return D2APIInterface.APIPost(path, body, token);
+
 };
 
 function GetLinkedProfiles(token, membership_id, membership_type){
@@ -25,4 +32,31 @@ function GetLinkedProfiles(token, membership_id, membership_type){
     return D2APIInterface.APIGet(path, token);
 };
 
-module.exports = {GetDestinyManifest, GetCredentialTypesForTargetAccount, GetMembershipDataById, GetLinkedProfiles};
+/*
+Takes the following parameters:
+access token
+a destiny membershipid
+a destiny membership type (BungieMembershipType enum)
+a list of components (Destiny.DestinyComponentType enum) Note: these should be a comma-separated list in the querystring parameter
+*/
+function GetProfile(token, destiny_membership_id, membership_type, components){
+    if(!membership_type) membership_type = "-1"; // -1 is the enum value for all membership types. hopefully remove this hardcoding later.
+    let path = api_root + "/Destiny2/"+membership_type+"/Profile/"+destiny_membership_id+"/";
+    let params = new URLSearchParams(components);
+    path = new URL(path);
+    path.search = params;
+    return D2APIInterface.APIGet(path, token);
+
+};
+
+function GetMembershipDataById(token, membership_id, membership_type){
+    if(!membership_type) membership_type = "-1"; // -1 is the enum value for all membership types. hopefully remove this hardcoding later.
+    let path = api_root+"/User/GetMembershipsById/"+membership_id+"/"+membership_type+"/";
+    return D2APIInterface.APIGet(path, token);
+};
+
+
+
+
+
+module.exports = {GetDestinyManifest, GetCredentialTypesForTargetAccount, GetMembershipDataById, GetLinkedProfiles, GetProfile, SearchDestinyPlayerByBungieName};
