4bb27a45d0df89816890c77121fd627e63346569
Trenton44 <38508613+Trenton44@users.noreply.github.com>
2022-10-15 16:57:36 -0500

    Added basic structure for character inventory to Structure.js

--- src/structure.js
+++ src/structure.js
@@ -3,37 +3,61 @@ import { Container, Row, Col, Tabs, Tab, Button } from 'react-bootstrap';
 import './structure.css';
 import Character from './character/character.js';
 import Vault from './vault/vault.js';
+import EquipmentGroup from './equipment-group/equipment-group';
+import CharacterCarousel from './character-carousel/character-carousel';
+
 class Structure extends React.Component {
     constructor(props){
         super(props);
-        this.state = {};
+        this.state = {
+            temp_group_count: 9,
+            degree_spread: 0,
+        };
     }
     render(){
         return(
-            <>
-            <Container fluid>
-                <Row>
-                    <p> Top Level </p>
+            <Container className="characters-grid-container" fluid>
+                <Row className="characters-grid-row">
+                    <Col><EquipmentGroup /></Col>
+                    <Col><EquipmentGroup /></Col>
+                    <Col><EquipmentGroup /></Col>
                 </Row>
-                <Row>
-                    <Col style={{"maxWidth": "50%"}}>
-                    <Tabs className='justify-content-center'>
-                        <Tab eventKey="character-1-tab" title="character-1"> 
-                            <Character />
-                        </Tab>
-                        <Tab eventKey="character-2-tab" title="character-2"> 
-                            <Character />
-                        </Tab>
-                        <Tab eventKey="character-3-tab" title="character-3"> 
-                            <Character />
-                        </Tab>
-                    </Tabs>
+                <Row className="characters-grid-row">
+                    <Col><EquipmentGroup /></Col>
+                    <Col>
+                        <CharacterCarousel />
                     </Col>
+                    <Col><EquipmentGroup /></Col>
+                </Row>
+                <Row className="characters-grid-row">
+                    <Col><EquipmentGroup /></Col>
+                    <Col><EquipmentGroup /></Col>
+                    <Col><EquipmentGroup /></Col>
                 </Row>
             </Container>
-            </>
         );
     }
 }
 
 export default Structure;
+
+/*
+<Container fluid>
+    <Navigation></Navigation>
+    <Row>
+        <Col style={{"maxWidth": "50%"}}>
+        <Tabs className='justify-content-center'>
+            <Tab eventKey="character-1-tab" title="character-1"> 
+                <Character />
+            </Tab>
+            <Tab eventKey="character-2-tab" title="character-2"> 
+                <Character />
+            </Tab>
+            <Tab eventKey="character-3-tab" title="character-3"> 
+                <Character />
+            </Tab>
+        </Tabs>
+        </Col>
+    </Row>
+</Container>
+*/
